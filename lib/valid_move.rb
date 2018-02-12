f5f886f68f106a5b1c4581d588ecb122e4358fbd
stevenshoemaker <steven.shoemaker@wework.com>
2018-02-12 01:56:15 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -14,6 +14,7 @@ def valid_move? (board,index)
     false
     elsif position_taken?(board,index) == false
     true
-  elsif position_taken?(board,index).between?(0,8) == true
+  elsif position_taken?(board,index) == 0 > || position_taken?(board,index) < 8
+  false
   end
 end
