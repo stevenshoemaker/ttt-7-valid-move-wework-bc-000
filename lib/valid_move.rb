fd315de892d30d1583a3ec7eea04604daaa0f857
stevenshoemaker <steven.shoemaker@wework.com>
2018-02-12 01:30:35 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,16 +1,17 @@
-def valid_move? (board,index)
-  if position_taken? 
-    elsif position_taken?.between?(0,8)
-    true
-  end
-end
-  
 def position_taken? (board,index)
 if board[index] == " " || board[index] == ""
     false
     elsif board[index] == nil
     false
  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+
+
+def valid_move? (board,index)
+  if position_taken? 
+    elsif position_taken?.between?(0,8)
     true
   end
 end
